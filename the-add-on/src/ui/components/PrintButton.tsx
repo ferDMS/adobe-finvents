15bc6a207e807fa42071874c2c8db99652ca6440
Daniel Monroy <fernandodms04@gmail.com>
2024-09-15 10:32:31 -0600

    Stable a bit more style

--- the-add-on/src/ui/components/PrintButton.tsx
+++ the-add-on/src/ui/components/PrintButton.tsx
@@ -14,16 +14,26 @@ const PrintButton: React.FC<PrintButtonProps> = ({ sandboxProxy, event }) => {
     const width = 1080;
 
     const handlePrintStatistics = () => {
+
+        // Print rectangle in the background
+        sandboxProxy.createBackgroundRectangle();
+
+        // Print rectangle as header of the background rectangle in a slightly darker shade
+        sandboxProxy.createRectangleCoords(0.4, 0.4823529411764706, 0.7764705882352941, width/8, height/4, width - (2*(width/8)), 100);
+
         const members = event.members;
         const spacing = 50;
         const n = members.length;
 
         const startY = height / 2 - (n * spacing) / 2;
 
+        // Order members by member.paid descending
+        members.sort((a, b) => b.paid - a.paid);
+
         members.forEach((member, index) => {
             const x = width / 4;
             const y = startY + index * spacing;
-            sandboxProxy.createTextCoords(`${index + 1}. ${member.user.name}`, x, y);
+            sandboxProxy.createTextCoords(`${index + 1}. ${member.user.name}`, x-50, y);
         });
 
         members.forEach((member, index) => {
